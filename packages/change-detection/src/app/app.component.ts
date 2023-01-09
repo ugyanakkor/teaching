ce7cfd54ebe690d64bacb8239aa53a62f6f4a0e4
Kovács Tibor <tibor.kovacs@quest.com>
2023-01-09 22:50:14 +0100

    feat(change-detection): Add change detector to app component

--- packages/change-detection/src/app/app.component.ts
+++ packages/change-detection/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'teaching-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'change-detection';
+
+  // 6. Change detector ref to manually turn off/on the change detection
+/*  constructor(private ref: ChangeDetectorRef) {
+    ref.detach();
+    setInterval(() => {
+      this.ref.detectChanges();
+    }, 5000);
+  }*/
 }
