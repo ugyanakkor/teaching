b72d8dab447715e6693ee52e01b142a741caef69
Kovács Tibor <ifjutibor@gmail.com>
2023-01-10 15:00:21 +0100

    feat(ngrx): Setup ngrx

--- packages/ngrx/src/app/app.module.ts
+++ packages/ngrx/src/app/app.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PostsComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
